46600ca5d42ed3cf6fe46e9ca3ae10036a2f61ab
Erlang Parasu <erlangparasu@gmail.com>
2024-07-29 16:25:07 +0800

    feat: hammerspoon: focus to Android Emulator

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-local hotkey = require 'hs.hotkey'
-local window = require 'hs.window'
-local application = require 'hs.application'
+local khotkey = require 'hs.hotkey'
+local kwindow = require 'hs.window'
+local kapplication = require 'hs.application'
 
+-- NOTE: Common Applications
 local key2app = {
     a = 'Alacritty.app',
     -- b = '',
@@ -33,11 +34,42 @@ local key2app = {
 
 -- Toggle an application between being the frontmost app, and being hidden
 local function toggle_application(_app)
-    application.launchOrFocus(_app)
+    kapplication.launchOrFocus(_app)
 end
 
 for key, app in pairs(key2app) do
-    hotkey.bind({ 'alt' }, key, function()
+    khotkey.bind({ 'alt' }, key, function()
         toggle_application(app)
     end)
 end
+
+-- NOTE: Android Emulator
+local function focus_to_android_emulator()
+  -- Get the list of all windows
+  local allWindows = kwindow.allWindows()
+
+  -- Filter the windows that contain the string "android emulator"
+  local androidEmulatorWindow = nil
+  for _, window in ipairs(allWindows) do
+      local title = window:title()
+      if title:lower():find("android emulator") then
+          androidEmulatorWindow = window
+          break
+      end
+  end
+
+  -- If the window is found, focus it
+  if androidEmulatorWindow then
+      androidEmulatorWindow:focus()
+  else
+      -- If the window is not found, show an error message
+      -- hs.alert.show("Window with 'android emulator' not found")
+  end
+end
+
+khotkey.bind({ 'alt' }, 'x', function()
+    focus_to_android_emulator()
+end)
+
+
+-- Eof
